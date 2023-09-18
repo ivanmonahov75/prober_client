b91f7800a615b220e45153d4623b41709a7ca741
ivanmonahov75 <ivanmonahov75@gmail.com>
2023-09-18 10:10:14 -0700

    fixed: video translation

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import datetime
 import socket
 import cv2
 import time
 from multiprocessing import Process
+import numpy as np
 
 
 def gui():  # will be used for setting up a gui
@@ -12,10 +14,10 @@ def com_server():  # pure low-level communication with server (on raspberry)
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect(('127.0.0.1', 9090))
     while True:
-        client_socket.send(b'imag ')
+        client_socket.send(b'image ')
         data = client_socket.recv(60000)
-        print(data)
-        time.sleep(0.06)
+        frame = cv2.imdecode(np.frombuffer(data, np.uint8), -1)
+        time.sleep(0.05)
 
 
 if __name__ == '__main__':
